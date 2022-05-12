8f811b66c41c4dd1aae620b8621debd6d0c86eb4
valguru <gurina.va@edu.spbstu.ru>
2022-05-13 01:09:07 +0300

    Добавьте файлы проекта.

---
+++ Task1/Matrix.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Matrix
+    {
+        //хранение данных
+        private int[,] data;
+
+        // Конструкторы
+
+        // конструктор с двумя параметрами целого типа – размерность матрицы
+        public Matrix(int nRows, int nCols)
+        {
+            Rows = nRows;
+            Columns = nCols;
+            data = new int[Rows, Columns];
+        }
+
+        // конструктор с параметром - двумерный массив
+        public Matrix(int[,] initData)
+        {
+            data = initData;
+            Rows = data.GetLength(0);
+            Columns = data.GetLength(1);
+        }
+
+        // доступ к элементам матрицы через свойство-индексатор
+        public int this[int i, int j]
+        {
+            get
+            {
+                if (i < Rows && j < Columns) return data[i, j];
+                else throw new FormatException("Ошибка! Вышли за рамки размера матрицы");
+            }
+            set
+            {
+                if (value is int) data[i, j] = value;
+                else throw new FormatException("Ошибка! Введено значение, которое не имеет тип int");
+            }
+        }
+
+        public int[,] GetMatrix => data;
+
+        // Размер матрицы доступен только для чтения через свойства
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
+
+        // Сортировка пузырьком в порядке возрастания
+        public static Matrix BubbleSortUp(Matrix matr, comparison comp)
+        {
+            int[] mas = comp(matr);
+            int flag = 1;
+            while (flag == 1)
+            {
+                flag = 0;
+                for (int i = 0; i < matr.Rows - 1; i++)
+                {
+                    if (mas[i] > mas[i + 1])
+                    {
+                        for (int j = 0; j < matr.Columns; j++)
+                        {
+                            int tmp = matr[i, j];
+                            matr[i, j] = matr[i + 1, j];
+                            matr[i + 1, j] = tmp;
+                        }
+                        int tmp1 = mas[i];
+                        mas[i] = mas[i + 1];
+                        mas[i + 1] = tmp1;
+                        flag = 1;
+                    }
+                }
+            }
+            return matr;
+        }
+
+        // Сортировка пузырьком в порядке убывания
+        public static Matrix BubbleSortDown(Matrix matr, comparison comp)
+        {
+            int[] mas = comp(matr);
+            int flag = 1;
+            while (flag == 1)
+            {
+                flag = 0;
+                for (int i = 0; i < matr.Rows - 1; i++)
+                {
+                    if (mas[i] < mas[i + 1])
+                    {
+                        for (int j = 0; j < matr.Columns; j++)
+                        {
+                            int tmp = matr[i, j];
+                            matr[i, j] = matr[i + 1, j];
+                            matr[i + 1, j] = tmp;
+                        }
+                        int tmp1 = mas[i];
+                        mas[i] = mas[i + 1];
+                        mas[i + 1] = tmp1;
+                        flag = 1;
+                    }
+                }
+            }
+            return matr;
+        }
+
+        public static int[] sum(Matrix matr)
+        {
+            int[] sum = new int[matr.Rows];
+            for (int i = 0; i < matr.Rows; i++)
+            {
+                sum[i] = 0;
+                for (int j = 0; j < matr.Columns; j++)
+                {
+                    sum[i] += matr[i, j];
+                }
+            }
+            return sum;
+        }
+        public static int[] max(Matrix matr)
+        {
+            int[] max = new int[matr.Rows];
+            for (int i = 0; i < matr.Rows; i++)
+            {
+                max[i] = 0;
+                for (int j = 0; j < matr.Columns; j++)
+                {
+                    if (matr[i, j] > max[i]) max[i] = matr[i, j];
+                }
+            }
+            return max;
+        }
+        public static int[] min(Matrix matr)
+        {
+            int[] min = new int[matr.Rows];
+            for (int i = 0; i < matr.Rows; i++)
+            {
+                min[i] = 1000000000;
+                for (int j = 0; j < matr.Columns; j++)
+                {
+                    if (matr[i, j] < min[i]) min[i] = matr[i, j];
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,98 @@
+﻿using Task1;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System;
+
+namespace Task1
+{
+    public delegate int[] comparison(Matrix matr);
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Введите матрицу");
+            Console.Write("Количество строк: ");
+            int Rows = int.Parse(Console.ReadLine());
+            Console.Write("Количество столбцов: ");
+            int Col = int.Parse(Console.ReadLine());
+            Matrix m = new Matrix(Rows, Col);
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Col; j++)
+                {
+                    Console.Write("m[{0},{1}] = ", i, j);
+                    m[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            while (true)
+            {
+                Console.WriteLine("\n___________________________________________________________________\n" + 
+                    "\nВаша матрица:");
+                ShowMatrix(m);
+
+                Console.WriteLine("\nВыберите тип сортировки:\n" +
+                    "Сравнение сумм элементов строк матрицы;\n" +
+                    "   1 - по возрастанию сумм\n" +
+                    "   2 - по убыванию\n" +
+                    "Сравнение максимальных элементов в строках матрицы;\n" +
+                    "   3 - по возрастанию максимального элемента\n" +
+                    "   4 - по убыванию максимального элемента\n" +
+                    "Сравнение минимальных элементов в строках матрицы;\n" +
+                    "   5 - по возрастанию минимального элемента\n" +
+                    "   6 - по убывания минимального элемента\n\n" +
+                    "   7 - Выход\n");
+
+                int menu = int.Parse(Console.ReadLine());
+
+                Matrix resmatrix = new Matrix(m.Rows, m.Columns);
+                comparison comp;
+                switch (menu)
+                {
+                    case 1:
+                        comp = new comparison(Matrix.sum);
+                        resmatrix = Matrix.BubbleSortUp(m, comp);
+                        break;
+                    case 2:
+                        comp = new comparison(Matrix.sum);
+                        resmatrix = Matrix.BubbleSortDown(m, comp);
+                        break;
+                    case 3:
+                        comp = new comparison(Matrix.max);
+                        resmatrix = Matrix.BubbleSortUp(m, comp);
+                        break;
+                    case 4:
+                        comp = new comparison(Matrix.max);
+                        resmatrix = Matrix.BubbleSortDown(m, comp);
+                        break;
+                    case 5:
+                        comp = new comparison(Matrix.min);
+                        resmatrix = Matrix.BubbleSortUp(m, comp);
+                        break;
+                    case 6:
+                        comp = new comparison(Matrix.min);
+                        resmatrix = Matrix.BubbleSortDown(m, comp);
+                        break;
+                    case 7:
+                        Environment.Exit(0);
+                        break;
+                }
+
+                Console.WriteLine("\nРезультат сортировки:");
+                ShowMatrix(resmatrix);
+            }
+        }
+
+        public static void ShowMatrix(Matrix matr)
+        {
+            for (int i = 0; i < matr.Rows; i++)
+            {
+                for (int j = 0; j < matr.Columns; j++)
+                {
+                    Console.Write(matr[i, j] + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
